5588eabaf0853b06391cf5fd3ce45724d389e77c
ChineduEjiogu1 <chineduejiogu@gmail.com>
2022-10-25 17:21:18 -0400

    5th draft

--- script.js
+++ script.js
@@ -9,65 +9,65 @@ class Fighter
 
   attack(target) 
   {
-    if(randomNum(1,6) === 4)
+    if(randomNum(1, 6) === 4)
     {
       let attackPoints = 5;
       target.healthPoints = target.healthPoints - attackPoints;
-      alert(`${this.name} attacked ${target.name} with a special attack with ${target.ability} ${attackPoints} damage! ${target.name} has ${target.healthPoints} health left!`)
+      alert(`${this.name} attacked ${target.name} with a special attack with ${target.ability} doing ${attackPoints} damage! ${target.name} has ${target.healthPoints} health points left!`)
     }
     else
     {
-      let attackPoints = randomNum(1,6);
+      let attackPoints = randomNum(1, 6)
       target.healthPoints = target.healthPoints - attackPoints;
-      alert(`${this.name} attacked ${target.name} with a special attack with ${target.ability} ${attackPoints} damage! ${target.name} has ${target.healthPoints} health left!`)
+      alert(`You attacked ${target.name} with ${this.attackPoints} attack points of damage, ${target.name} has ${target.healthPoints} health points left!`)
     }
   }
 }
 
 class Hero extends Fighter 
 {
-  constructor(name, healthPoints, attackPoints) 
+  constructor(name, healthPoints, ability, attackPoints) 
   {
-    super(name, healthPoints, attackPoints);
-    this.ability = "Axe";
+    super(name, healthPoints, attackPoints)
+    this.ability = ability 
   }
 
   weapon() {
-    return `${this.name} is using a ${this.weapon}`;
+    return `${this.name} is using a ${this.weapon}`
   }
 }
 class Monster extends Fighter 
 {
-  constructor(name, healthPoints, attackPoints) 
+  constructor(name, healthPoints, ability, attackPoints) 
   {
     super(name, healthPoints, attackPoints);
-    this.ability = "shape-shift";
+    this.ability = ability 
   }
 
   ability() 
   {
-    return `${this.name} is using ${this.ability}`;
+    return `${this.name} is using ${this.ability}`
   }
 }
 
 function randomNum(min, max) 
 {
   //return a random integer between min - max
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
 }
 
-let myMonster = new Monster("Boogyman", 15);
-console.log(myMonster);
+let myMonster = new Monster("Boogyman", 15, "Laser-beams")
+console.log(myMonster)
 
-let myHero = new Hero("Superman", 15);
+let myHero = new Hero("Superman", 15, "shape-shift")
 
-console.log(myHero);
+console.log(myHero)
 
 function playRound() {
   //use randomNum to return either 0 or 1
-  let playerTurn = randomNum(0, 2);
+  let playerTurn = randomNum(0, 2)
   //0 = player goes first, 1 = monster goes first
 
   //use if/else to determine who goes first
@@ -77,18 +77,18 @@ function playRound() {
   //if monster goes first, run monsterAttack, then if playerHealth > 0, run playerAttack
 
   if (playerTurn === 0) {
-    myHero.attack(myMonster);
+    myHero.attack(myMonster)
 
     if (myMonster.healthPoints > 0) {
-      myMonster.attack(myHero);
+      myMonster.attack(myHero)
     }
   }
 
   if (playerTurn === 1) {
-    myMonster.attack(myHero);
+    myMonster.attack(myHero)
 
     if (myHero.healthPoints > 0) {
-      myHero.attack(myMonster);
+      myHero.attack(myMonster)
     }
   }
 }
@@ -113,12 +113,12 @@ function playGame() {
     playRound();
   }
   //outside of while loop, declare a winner and use alert to show a win or lose message
-  if (myHero.healthPoints > 0 && myMonster.healthPoints < 0) {
-    alert(`${myHero.name} you win ${myMonster.name} you lose`);
+  if (myHero.healthPoints >= 0 && myMonster.healthPoints <= 0) {
+    alert(`${myHero.name} you win ${myMonster.name} you lose`)
   }
 
-  if (myMonster.healthPoints > 0 && myHero.healthPoints < 0) {
-    alert(`${myMonster.name} you win ${myHero.name} you lose`);
+  if (myMonster.healthPoints >= 0 && myHero.healthPoints <= 0) {
+    alert(`${myMonster.name} you win ${myHero.name} you lose`)
   }
 }
 
